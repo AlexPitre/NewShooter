7861439f222bc3fdc5aa1807a383eb3a4a51c1c1
AlexPitre <ay.a.pea@gmail.com>
2018-10-01 21:42:58 -0400

    spawning

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -8,11 +8,11 @@ public class PlayerHealth : MonoBehaviour
 {
     public int startingHealth = 100;
     public int currentHealth;
-    public Slider healthSlider;
-    public Image damageImage;
+    //public Slider healthSlider;
+    //public Image damageImage;
     public AudioClip deathClip;
-    public float flashSpeed = 5f;
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+    //public float flashSpeed = 5f;
+    //public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 
 
     Animator anim;
@@ -33,18 +33,18 @@ public class PlayerHealth : MonoBehaviour
     }
 
 
-    void Update ()
-    {
-        if(damaged)
-        {
-            damageImage.color = flashColour;
-        }
-        else
-        {
-            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-        }
-        damaged = false;
-    }
+   // void Update ()
+   // {
+      //  if(damaged)
+       // {
+       //     damageImage.color = flashColour;
+       // }
+      //  else
+       // {
+       //     damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+       // }
+       // damaged = false;
+   // }false
 
 
     public void TakeDamage (int amount)
@@ -53,7 +53,7 @@ public class PlayerHealth : MonoBehaviour
 
         currentHealth -= amount;
 
-        healthSlider.value = currentHealth;
+        //healthSlider.value = currentHealth;
 
         playerAudio.Play ();
 
@@ -69,12 +69,13 @@ public class PlayerHealth : MonoBehaviour
         isDead = true;
 
         //playerShooting.DisableEffects ();
-
+		//player animator control receive die trigger, triggers death audio
         anim.SetTrigger ("Die");
 
         playerAudio.clip = deathClip;
         playerAudio.Play ();
 
+		//dont allow player to receive external force
         playerMovement.enabled = false;
         //playerShooting.enabled = false;
     }
